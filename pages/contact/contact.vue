c7a5e07442197e7404e17f00797cc06159e4e2f4
liaolonghui <1340881930@qq.com>
2021-04-24 18:59:36 +0800

    联系页以及社区图片页

--- pages/contact/contact.vue
+++ pages/contact/contact.vue
@@ -1,6 +1,15 @@
 <template>
-	<view>
-		contact
+	<view class="contact">
+		<image class="img" src="../../static/gywmban.jpg"></image>
+		<view class="info">
+			<view @click="makePhoneCall">
+				联系电话：400-168-9090（点击拨打）
+			</view>
+			<view>
+				校区地址：浙江省杭州市下沙经济开发区4号大街187号盛泰时代山
+			</view>
+		</view>
+		<map class="map" :scale="scale" :latitude="latitude" :longitude="longitude" :markers="markers"></map>
 	</view>
 </template>
 
@@ -8,15 +17,47 @@
 	export default {
 		data() {
 			return {
-				
+				scale: 17,
+				latitude: 30.312419,
+				longitude: 120.363118,
+				markers: [
+					{
+						latitude: 30.312419,
+						longitude: 120.363118,
+						iconPath: "../../static/hmlogo.png",
+						width: 30,
+						height: 30
+					}
+				]
 			}
 		},
 		methods: {
-			
+			makePhoneCall () {
+				uni.makePhoneCall({
+					phoneNumber: '400-168-9090'
+				})
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+.contact {
+	.img {
+		width: 750rpx;
+		height: 360rpx;
+	}
+	.info {
+		font-size: 28rpx;
+		padding: 10rpx;
+		view {
+			line-height: 80rpx;
+			border-bottom: 1px solid #eee;
+		}
+	}
+	.map {
+		width: 750rpx;
+		height: 750rpx;
+	}
+}
 </style>
--- pages/pics/pics.vue
+++ pages/pics/pics.vue
@@ -1,6 +1,16 @@
 <template>
-	<view>
-		pics
+	<view class="pics">
+		<scroll-view scroll-y class="left">
+			<view :class="{ 'active': i === index }" v-for="(item, i) in picsList" :key="i" @click="changeIndex(i)">
+				{{item.floor_title.name}}
+			</view>
+		</scroll-view>
+		<scroll-view scroll-y="true" class="right" v-if="picsList.length">
+			<view v-for="(item) in picsList[index].product_list" :key="item.name" >
+				<image @click="previewImage(item.image_src)" :src="item.image_src"></image>
+				<text>{{item.name}}</text>
+			</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -8,15 +18,75 @@
 	export default {
 		data() {
 			return {
-				
+				picsList: [],
+				index: 0
 			}
 		},
 		methods: {
-			
+			async getPicsData () {
+				const picsData = await this.$request("/home/floordata")
+				this.picsList = [
+					...picsData.data.message,
+					...picsData.data.message,
+					...picsData.data.message,
+					...picsData.data.message,
+					...picsData.data.message
+				]
+			},
+			// changeIndex
+			changeIndex (index) {
+				this.index = index
+			},
+			// previewImage
+			previewImage (current) {
+				const urls = this.picsList[this.index].product_list.map(item => item.image_src)
+				console.log(urls)
+				uni.previewImage({
+					current,
+					urls,
+					indicator: "number",
+					loop: "true"
+				})
+			}
+		},
+		onLoad() {
+			this.getPicsData()
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+page {
+	height: 100%;
+}
+.pics {
+	height: 100%;
+	display: flex;
+	.left {
+		width: 200rpx;
+		height: 100%;
+		view {
+			height: 120rpx;
+			line-height: 120rpx;
+			text-align: center;
+			font-size: 30rpx;
+			color: #333;
+			border-bottom: 1px solid #ddd;
+			border-right: 1px solid #ddd;
+		}
+		.active {
+			color: #fff;
+			background-color: $shop-color;
+		}
+	}
+	.right {
+		width: 550rpx;
+		text-align: center;
+		image {
+			display: block;
+			width: 90%;
+			margin: 20rpx auto;
+		}
+	}
+}
 </style>
